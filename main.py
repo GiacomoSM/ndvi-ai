6bacf8e542fd7d443b67fd5a89143e06d7a9fa51
GiacomoSM <116431948+GiacomoSM@users.noreply.github.com>
2022-11-14 20:47:53 +0100

    Create main.py
    This is the main file for the AI. PLEASE CHECK ALL CHANGES BEFORE COMMITING CHANGES TO THE MAIN BRANCH.

---
+++ main.py
@@ -0,0 +1,22 @@
+# IMPORTS
+import time
+# CUSTOMIZATION
+AI_NAME_FIRST = "Cody"
+AI_NAME_MIDDLE = "Benjamin"
+AI_NAME_LAST = "Robinson"
+# PERSONALITY
+# The available personalitys are: 
+# SARCASTIC, RELAXED, AGGRESSIVE, TIMID, INTELLIGENT, KNOW_IT_ALL
+PERSONALITY = "SARCASTIC"
+# CORE VARIABLES ABOUT THE AI
+AI_VERSION = "0.1"
+AI_DESCRIPTION = "talkAI, also known as Cody Benjamin Robinson, is a simple AI that simulates the behavior of a human and is designed to be used as a starting point for a new AI. talkAI doesnt have very much training, so this replit version is the frame for something much, much, bigger. talkAI is just the glimpse of the horizon for intelligent computer AIs and the goal of the project is to create a modern framework for conversation AIs that, after hopefully years and years of training, begin to sound like a real human. The AI is free to modify and share, as well as reference in books, websites, or any media form in any way. This is NOT a commercial AI, and it is STRICTLY PROHIBITED to be distriubted as a commercial AI. talkAI may be used commercially, for example, as a company's feedback bot, but is NOT A PAID SERVICE AND WILL NOT BE DISTRIBUITED IN ANY WAY, WHOLE OR PARTIAL, TO ANY PERSON OR ENTITY FOR A PRICE. These terms may seem strict, and you may want to use your AI that you trained for commercial purposes, but please get approval from the team before distributing it. This helps keep talkAI a free and open-source project and not a product. If you are interested in contributing to the talkAI project, please join us on the talkAI github repo."
+AI_AUTHOR = "Giacomo S.M."
+AI_AUTHOR_EMAIL = "talkingslug@duck.com"
+AI_KILLCODE = "KILL_AI_GSKILLCODE" # < < < Whats This? The kill code is a code that may be used to shut down the AI completely, essentially, killing it. This is not a security code, like a passcode, but rather a word that stops it in case of an error.
+# Turn AI_DEBUG to false to disable response time logging, killcode and other debug messages such as how many results were found as an awnser for a message. 
+AI_DEBUG = True
+# Turn AI_INPRODUCTION to true ONLY WHEN COMPLETELY DONE WITH THE AI. This turns off many layers of error handling, so only do this once you have checked the AI rigourously many times and are confident that it is working. DO NOT edit any code, even if you are only changing the name of the AI, while AI_INPRODUCTION is true.
+AI_INPRODUCTION = False
+
+# MAIN
